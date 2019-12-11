8f66de28ccd19f04829e8af974157bbf98ef0769
Aviv Haliva <aviv.haliva@gmail.com>
2019-12-11 17:42:36 +0200

    refactor reader so it is iterable

--- Brain-Overflow/utils/reader.py
+++ Brain-Overflow/utils/reader.py
@@ -1,16 +1,22 @@
 import struct 
 import os
+import datetime as dt
 
 USER_ID_BYTES_SIZE = 8
+USER_INFO_FIXED_SIZE = 17
 SNAPSHOT_FORMAT = 'ldddddddII'
 
 class Reader:
 	def __init__(self, path):
 		self.path = path
+		self.offset = 0
 		with open(self.path, "rb") as file:
 			self.extract_user_info(file)
-			print(os.stat(path).st_size)
-			self.process_snapshots(file)
+	
+	def __iter__(self):
+		with open(self.path, "rb") as file:
+			for i in self.process_snapshots(file):
+				yield i
 
 	def read_var_in_requested_format(self, file, bin_length, requested_format, decode=False):
 		var_bin_rep = file.read(bin_length)
@@ -27,36 +33,35 @@ class Reader:
 		self.user_name = self.read_var_in_requested_format(file, user_name_size, '{0}s'.format(user_name_size), True)
 		self.user_birth_date = self.read_var_in_requested_format(file, 4, 'i')
 		self.user_gender = self.read_var_in_requested_format(file, 1, 'c', True)
+		self.offset = USER_INFO_FIXED_SIZE + user_name_size
 
 	def gen_next_snapshot_ts(self, file):
-		data = self.read_var_in_requested_format(file, 8, 'l')
-		while data:
-			yield data
-			data = self.read_var_in_requested_format(file, 8, 'l')
-		return
+		file.seek(self.offset)
+		try:
+			data = self.read_var_in_requested_format(file, 8, 'Q')
+			while data:
+				yield data
+				data = self.read_var_in_requested_format(file, 8, 'Q')
+		finally:
+			return
 
 	def process_snapshots(self, file):
 		ts_gen = self.gen_next_snapshot_ts(file)
 		for ts in ts_gen:
-			print('gen next')
+			datetime = dt.datetime.fromtimestamp(ts/1000.0)
 			translation = self.read_var_in_requested_format(file, 24, 'ddd')
 			rotation = self.read_var_in_requested_format(file, 32, 'dddd')
-
 			color_image_dimension = self.read_var_in_requested_format(file,8, 'II')
 			color_image_size = color_image_dimension[0] * color_image_dimension[1]
-			file.seek(color_image_size*3)
-			#color_image_vals = file.read(color_image_size*3)
+			color_image_vals = file.read(color_image_size*3)
 			depth_image_dimension = self.read_var_in_requested_format(file,8, 'II')
 			depth_image_size = depth_image_dimension[0] * depth_image_dimension[1]
-			#Memory error problem:
-			#should unpack to floats somehow!
-			#depth_image_vals = self.read_var_in_requested_format(file, depth_image_size, '{0}f'.format(depth_image_size))
-			#depth_image_vals = file.read(depth_image_size)
-			file.seek(depth_image_size)
+			depth_image_vals = file.read(depth_image_size*4)
 			user_feelings = self.read_var_in_requested_format(file, 4*4, 'ffff')
-			#Create snapshot object?
-			print ('-------------')
-			print(file.tell())
+			#TODO - Create snapshot object?
+			yield datetime
 
 x = Reader('sample.mind')
+for s in x:
+	print(s)
 
