0c11ad57e5b0a705fbc96632022bae7970bfc579
Aviv Haliva <aviv.haliva@gmail.com>
2020-04-19 17:02:12 +0300

    add client tests

--- brain_overflow/client/client.py
+++ brain_overflow/client/client.py
@@ -7,14 +7,21 @@ import json
 
 GET_CONFIG_REQUEST = 'http://{0}:{1}/config'
 UPLOAD_SNAPSHOT_REQUEST = 'http://{0}:{1}/upload-snapshot'
+CLIENT_ERROR_MESSAGE = 'Error in client upload_sample:'
 
 def upload_sample(path, host, port, file_format='protobuf'):
-    reader = Reader(path, file_format)
+	try:
+	    reader = Reader(path, file_format)
 
-    for snapshot in reader:
-        user = reader.user
-        response = requests.get(GET_CONFIG_REQUEST.format(host,port))
-        fields = json.loads(response.content)['fields']
+	    for snapshot in reader:
+	        user = reader.user
+	        response = requests.get(GET_CONFIG_REQUEST.format(host,port))
+	        fields = json.loads(response.content)['fields']
 
-        requests.post(UPLOAD_SNAPSHOT_REQUEST.format(host,port),
-        data = gen_client_message(user, snapshot, fields))
+	        requests.post(UPLOAD_SNAPSHOT_REQUEST.format(host,port),
+	        data = gen_client_message(user, snapshot, fields))
+	        
+	except Exception as e:
+		print(CLIENT_ERROR_MESSAGE)
+		print(e)
+		exit(1)
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,30 @@
 import pytest
+import unittest
+from brain_overflow.client import upload_sample
 
-def test_no_such_server():
-	assert 1 == 1
+class TestClient(unittest.TestCase):
+	def test_no_server_exit_grcefully(self):
+		with self.assertRaises(SystemExit) as cm:
+			upload_sample(host='127.0.0.1', port=8000, path='sample.mind.gz')
+		self.assertEqual(cm.exception.code, 1)
+
+	def test_no_such_file_exit_grcefully(self):
+		with self.assertRaises(SystemExit) as cm:
+			upload_sample(host='127.0.0.1', port=8000, path='no_such_file.gz')
+		self.assertEqual(cm.exception.code, 1)
+
+	def test_no_exception_is_raised(self):
+	    try:
+	        upload_sample(host='127.0.0.1', port=8000, path='no_such_file.gz')
+	    except SystemExit:
+	    	pass
+	    except Exception:
+	        self.fail("myFunc() raised Exception unexpectedly!")
+
+	def test_reader_results(self):
+		upload_sample(host='127.0.0.1', port=8000, path='sample.mind.gz')
+
+
+
+	 
+
