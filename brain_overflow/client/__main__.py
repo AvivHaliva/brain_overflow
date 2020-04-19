92f55890925254ff8d506d49c6816bd751159846
Aviv Haliva <aviv.haliva@gmail.com>
2020-04-19 17:13:18 +0300

    flake8d client

--- brain_overflow/client/__main__.py
+++ brain_overflow/client/__main__.py
@@ -1,19 +1,34 @@
 import click
 from . import client
 
+
 @click.group()
 def cli():
     pass
 
+
 @cli.command('upload-sample')
 @click.argument('path', type=click.Path(exists=True))
-@click.option('-h', '--host',default='127.0.0.1', show_default=True, type=str, help='server ip')
-@click.option('-p', '--port', default=8000, show_default=True, type=int, help='server port')
+@click.option(
+    '-h',
+    '--host',
+    default='127.0.0.1',
+    show_default=True,
+    type=str,
+    help='server ip')
+@click.option(
+    '-p',
+    '--port',
+    default=8000,
+    show_default=True,
+    type=int,
+    help='server port')
 @click.argument('file_format', type=str)
 def upload_sample_command(path, host, port, file_format):
-	""" reads a sample from specified PATH in the specified FILE_FORMAT
-		and uploads it to the server = (host, port)"""
-	client.upload_sample(path, host, port, file_format)
+    """ reads a sample from specified PATH in the specified FILE_FORMAT
+            and uploads it to the server = (host, port)"""
+    client.upload_sample(path, host, port, file_format)
+
 
 cli.add_command(upload_sample_command)
 
